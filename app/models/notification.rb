a150f5e8da56a302f001abcbeb23a8d104432503
Marcus Pereira <marcus.work@hotmail.com>
2023-08-27 16:37:57 -0300

    Setting channel

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,15 @@
 class Notification < ApplicationRecord
+  after_create :stream_notification
+
   belongs_to :user
+
+  validates :message, presence: true
+
+  private
+
+  def stream_notification
+    message_data = { user_name: user.name, message: message }
+
+    ActionCable.server.broadcast("notifications_#{user_id}", message_data)
+  end
 end
